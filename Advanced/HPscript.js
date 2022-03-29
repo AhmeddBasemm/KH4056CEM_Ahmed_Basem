dcd1ff39b290894187deece491b0e1ff065b7cb3
AhmeddBasemm <ahmedbasemegy@gmail.com>
2022-03-29 16:12:16 +0200

    HP

--- Advanced/HPscript.js
+++ Advanced/HPscript.js
@@ -1,18 +1,19 @@
+// Get All Elements with the accordion Class
 var acc = document.getElementsByClassName("accordion");
-var i;
+var i; //Looping Variable
 
+// Loop through the accordion class elements
 for (i = 0; i < acc.length; i++) {
-acc[i].addEventListener("click", function() {
-  /* Toggle between adding and removing the "active" class,
-  to highlight the button that controls the panel */
-  this.classList.toggle("active");
+    // add a click Event Listener to the elemnt
+    acc[i].addEventListener("click", function() {
 
-  /* Toggle between hiding and showing the active panel */
-  var panel = this.nextElementSibling;
-  if (panel.style.display === "block") {
-    panel.style.display = "none";
-  } else {
-    panel.style.display = "block";
-  }
-});
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      // Toggle between hiding and showing the active panel
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") panel.style.display = "none";
+      else panel.style.display = "block";
+  });
 }
